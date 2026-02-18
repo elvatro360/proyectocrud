265238abe20fbf09986a11a9e3cd6a657204f20d
elvatro360 <fmtz8494@gmail.com>
2026-02-18 08:20:58 -0600

    Proyecto CRUD Django

---
+++ cruDapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_productos, name='lista_productos'),
+    path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+    path('producto/nuevo/', views.crear_producto, name='crear_producto'),
+    path('producto/<int:pk>/editar/', views.editar_producto, name='editar_producto'),
+    path('producto/<int:pk>/borrar/', views.borrar_producto, name='borrar_producto'),
+]
